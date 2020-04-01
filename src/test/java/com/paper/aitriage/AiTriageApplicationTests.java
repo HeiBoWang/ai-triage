f5a4473bc0de1d7048b9f9c7ab83795795a4d302
wangyongpeng11 <helloPWD.520>
2020-04-01 19:01:03 +0800

    init

---
+++ src/test/java/com/paper/aitriage/AiTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paper.aitriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
